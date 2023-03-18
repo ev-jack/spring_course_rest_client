21d4d2014bc86f837cc0d158d965b76a699a7439
ev-jack <jolyjeck@gmail.com>
2023-03-18 20:54:41 +0200

    added gets methods

---
+++ src/main/java/com/yevhen/spring/App.java
@@ -0,0 +1,26 @@
+package com.yevhen.spring;
+
+import com.yevhen.spring.rest.Communication;
+import com.yevhen.spring.rest.configuration.MyConfig;
+import com.yevhen.spring.rest.entity.Employee;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(MyConfig.class);
+
+        Communication communication = context.getBean("communication",
+                Communication.class);
+
+        List<Employee> allEmployees = communication.getAllEmployees();
+        System.out.println(allEmployees);
+
+        Employee empByID = communication.getEmployee(5);
+        System.out.println(empByID);
+
+    }
+}
